9725390da3c2523550dea80bb83c1c8e3dd99014
arthdarji352 <arthdarji20@gmail.com>
2023-10-20 11:12:22 +0530

    project

---
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Username = () => {
+  return (
+    <div className=" hidden text-sm font-semibold md:block">arth darji</div>
+  );
+};
+
+export default Username;
